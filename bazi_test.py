be01c4f63112d70ee45614c7ef7d7c7fc9fc0ed7
KittenCN <owdely@vip.163.com>
2023-12-13 10:41:09 +0800

    update

--- bazi_test.py
+++ bazi_test.py
@@ -1,2 +1,13 @@
-from bazi import bazi
+import datetime
+import sys
+sys.path.append(r'./bazi') # 將上一層路徑加入模組搜尋路徑
+import bazi
 
+now = datetime.datetime.now()
+options = bazi.option()
+options.year = 1985
+options.month = 9
+options.day = 5
+options.time = 9
+options.n = True
+bazi.main(options)
